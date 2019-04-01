a155e9839daa2b5dc675d12c4b0f178cfaec2bc1
Ian Smith <ianmsmith17@gmail.com>
2019-04-01 15:31:53 -0500

    started transferring pseudo code

---
+++ js/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var correct = 0;
+var incorrect = 0;
+
+var questions = [
+  {text: 'Question 1', answer: 'Correct', options: []}
+];
+var results = [];
+
+function displayQuestion() {
+
+}
+
+function verify() {
+
+}
+
+function displayStats() {
+
+}
+
+function startQuiz() {
+  for(var i = 0; i <questions.length; i++) {
+    var ans = displayQuestion();
+  }
+}

27dee645d6d36090e1bf58d56afe288d97b80a57
Floyd Orr <floydcanhelp@gmail.com>
2019-04-02 17:25:11 -0500

    basic logic works

--- js/app.js
+++ js/app.js
@@ -4,19 +4,17 @@
 
 var quizArray = [
   {text: `This Iowan often nest in colonies,and in winter this bird feeds on left-over grain in fields</p><p> This bird breeds from late May to mid-August. <p>Which bird is it hugh?</p>`, answer: 'Snow Goose',yourAnswer: '', trueFalse:'', options: ['Blackpoll Warbler', 'White-fronted Goose', 'Ross\'s Goose','Snow Goose']},
-  {text: `<p> THis bird is very elusive and seldom seen</p><p>It's nest is a shallow cup built with marsh vegetation on damp ground under a canopy of dead plants</p><p>eggs are creamy, and spotted with both reddish spots that form a ring at one end</p> <p>Which bird is it hugh?</p> `, answer: 'Yellow rail', yourAnswer: 'Ruby-throated hummingbird',  trueFalse:'', options: ['Yellow rail', 'Ruby-throated hummingbird', 'Ovenbird','Chimney Swift']},
+  {text: `<p> THis bird is very elusive and seldom seen</p><p>It's nest is a shallow cup built with marsh vegetation on damp ground under a canopy of dead plants</p><p>eggs are creamy, and spotted with both reddish spots that form a ring at one end</p> <p>Which bird is it hugh?</p> `, answer: 'Yellow rail', yourAnswer: '',  trueFalse:'', options: ['Yellow rail', 'Ruby-throated hummingbird', 'Ovenbird','Chimney Swift']},
   {text: `<p>This bird 2.8 to 3.5 inches long</p>,<p>spending most of the winter in Florida, southern Mexico and Central America,[9] as far south as extreme western Panama, and the West Indies.</p> <p>backward, and to hover in mid-air`,answer: 'Ruby-throated hummingbird', yourAnswer: '', trueFalse:'', options: ['Painted Bunting', 'Ruby-throated hummingbird', 'Ovenbird','Horned grebe']},
-  {text: `<p> This bird is a superb aerialist, and only rarely seen at rest.It can fly 10 months non stop. It drinks on the wing, skimming the surface of the water with its beak</p> <p>has been recorded by pilots flying more than a mile above the surface<<p>Which bird is it hugh?</p> `, answer: 'Chimney Swift', yourAnswer: '', trueFalse:'', options: ['Mourning Dove', 'Yellow Rail', 'Ovenbird','Chimney Swift']}];
+  {text: `<p> This bird is a superb aerialist, and only rarely seen at rest.It can fly 10 months non stop. It drinks on the wing, skimming the surface of the water with its beak</p> <p>has been recorded by pilots flying more than a mile above the surface<p>Which bird is it hugh?</p> `, answer: 'Chimney Swift', yourAnswer: '', trueFalse:'', options: ['Mourning Dove', 'Yellow Rail', 'Ovenbird','Chimney Swift']}];
 var correct = 0;
 var incorrect = 0;
 var num = 0;
 var currentQuiz;
-var results = [];
 var displayText = document.querySelector('#questions');
 var buttons = document.querySelectorAll('button');
-for(var button of buttons) {
-  button.addEventListener('click', quizSelect);
-}
+
+
 // get object properties and display on page (return answer property of object)
 function displayQuestion(QuizQuestion) {
   // code to display buttons and questions on page will go here
@@ -42,71 +40,53 @@ function verify(clickedAnswer) {
     correct++;
     currentQuiz.trueFalse ='true';
     var isCorrect = true;
-    alert('true');
-    startQuiz();
+    // document.querySelector("'#'+clickedAnswer");
+    num++;
+    console.log(num, quizArray.length);
+    if(num  === quizArray.length) {
+      displayStats();
+    } else{
+      startQuiz();
+    }
   } else {
-    isCorrect =false;
+    isCorrect ='false';
     incorrect++;
     currentQuiz.trueFalse ='false';
-    startQuiz();
+    num++;
+    if(num  === quizArray.length) {
+      displayStats();
+    } else{
+      startQuiz();
+    }
   }
-
+  
 
   return isCorrect;
 }
 
 // displays count of correct and incorrect, possibly more info in future
 function displayStats() {
-  if(num === quizArray.length) {
-    displayText.innerHTML =
-
-    `<pre> 
-       You got ${correct} answer right and ${incorrect} wrong.
-       For Question: ${currentQuiz.text}
-       Your Answer: ${currentQuiz.yourAnswer}
-       Correct Answer ${currentQuiz.answer}
-       True/Fase: ${currentQuiz.trueFalse}
-
-
-                                   
+  // if(num === quizArray.length) {
+  displayText.innerHTML =`<pre> 
+       You got ${correct} answer right and ${incorrect} wrong.                             
     </pre>`;
-
-  
-  }
+  // }
 
 }
 
-function thisResult() {
 
-}
 //shows which button was clicked
 function quizSelect(event) {
   var buttonTextValue = event.target.innerText;
   verify(buttonTextValue);
-  // var buttons = document.querySelectorAll('button');
-  // for(var button of buttons) {
-  //   button.addEventListener('click',function(event){
-  //     var buttonTextValue = event.target.innerText;
-  //     console.log(event.target.innerText, ' text of button clicked');
-  //     verify(buttonTextValue);
-  //   });
-  
-
 }
+
 function startQuiz() {
-  console.log('num ', num);
+  for(var button of buttons) {
+    button.addEventListener('click', quizSelect);
+  }
   displayQuestion(quizArray[num]);
-  num++;
-  return 0;
-
-  // var num = 0;
-  // for (var i = 0; i < questions.length; i++) {
-  // passes current queston as parameter, gets answer,
-  //works
-  // passes answer as parameter for verify(), and binds to result
-  //var result = verify(displayQuestion(questions[i]));
-
-  //}
+  
 }
 //quizSelect();
 

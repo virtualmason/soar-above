a8c6d3e6dc873cf40f1fe0cefcac219c8df2d77c
Floyd Orr <floydcanhelp@gmail.com>
2019-04-02 13:05:02 -0500

    added basic logic

--- js/app.js
+++ js/app.js
@@ -2,31 +2,55 @@
 'use strict';
 
 
-var correct = 0;
-var incorrect = 0;
-
-var questions = [
-  {text: `This Iowan often nest in colonies,and in winter this bird feeds on left-over grain in fields</p><p> This bird breeds from late May to mid-August`, answer: 'snow goose', options: ['Blackpoll warbler', 'White-fronted Goose', 'Ross\'s Goose','snow goose']},
-  {text: 'This bird feeds primarily on flying insects, but also on airborne spiders,has been recorded by pilots flying more than a mile above the surface,a superb aerialist, and only rarely seen at rest. It drinks on the wing, skimming the surface of the water with its beak ', answer: 'Chimney swift', options: ['Mourning dove, ', 'Yellow rail', 'Ovenbird','Chimney swift']},
+var quizArray = [
+  {text: `This Iowan often nest in colonies,and in winter this bird feeds on left-over grain in fields</p><p> This bird breeds from late May to mid-August. <p>Which bird is it hugh?</p>`, answer: 'Snow Goose', options: ['Blackpoll Warbler', 'White-fronted Goose', 'Ross\'s Goose','Snow Goose']},
+  {text: `<p>This bird feeds primarily on flying insects, but also on airborne spiders,has been recorded by pilots flying more than a mile above the surface</p>,<p>a superb aerialist, and only rarely seen at rest. It drinks on the wing, skimming the surface of the water with its beak</p> <p>Which bird is it hugh?</p> `, answer: 'Chimney Swift', options: ['Mourning Dove', 'Yellow Rail', 'Ovenbird','Chimney Swift']},
   {text: 'Question 3', answer: 'Correct', options: ['Incorrect', 'Incorrect', 'Incorrect']},
   {text: 'Question 4', answer: 'Correct', options: ['Incorrect', 'Incorrect', 'Incorrect']}
 ];
-
+var correct = 0;
+var incorrect = 0;
+var num = 0;
+var currentQuiz;
 var results = [];
-
+var buttons = document.querySelectorAll('button');
+for(var button of buttons) {
+  button.addEventListener('click', quizSelect);
+}
 // get object properties and display on page (return answer property of object)
-function displayQuestion(question) {
-  var text = question.text;
-  // code to display on page will go here
+function displayQuestion(QuizQuestion) {
+  console.log(QuizQuestion, '22');
+  var text = QuizQuestion.text;
+  // code to display buttons and questions on page will go here
   var displayText = document.querySelector('#questions');
-  // console.log(displayText);
+  var buttonOne = document.querySelector('#buttonOne');
+  var buttonTwo = document.querySelector('#buttonTwo');
+  var buttonThree = document.querySelector('#buttonThree');
+  var buttonFour = document.querySelector('#buttonFour');
+
+  // console.log(displayText);questions[0].options[0
   displayText.innerHTML =text;
-  // return question.answer;
+  buttonOne.innerHTML = QuizQuestion.options[0];
+  buttonTwo.innerHTML = QuizQuestion.options[1];
+  buttonThree.innerHTML = QuizQuestion.options[2];
+  buttonFour.innerHTML = QuizQuestion.options[3];
+  currentQuiz = QuizQuestion;
+  return QuizQuestion;
 }
 
 // checks if selected answer is correct, returns bool to startQuiz
-function verify(result) {
-  var isCorrect;
+function verify(clickedAnswer) {
+  console.log('verify', currentQuiz.answer );
+  if(clickedAnswer === currentQuiz.answer) {
+    correct++;
+    var isCorrect = true;
+    alert('true');
+    startQuiz();
+  } else {
+    isCorrect =false;
+    incorrect++;
+    startQuiz();
+  }
 
 
   return isCorrect;
@@ -41,30 +65,37 @@ function thisResult() {
 
 }
 //shows which button was clicked
-function quizSelect() {
-  var buttons = document.querySelectorAll('button');
-  for(var button of buttons) {
-    button.addEventListener('click',function(event){
-      console.log(event.target.id, ' but')
-    });
-
-  }
+function quizSelect(event) {
+  var buttonTextValue = event.target.innerText;
+  verify(buttonTextValue);
+  // var buttons = document.querySelectorAll('button');
+  // for(var button of buttons) {
+  //   button.addEventListener('click',function(event){
+  //     var buttonTextValue = event.target.innerText;
+  //     console.log(event.target.innerText, ' text of button clicked');
+  //     verify(buttonTextValue);
+  //   });
   
+
 }
 
-// main function
+
 function startQuiz() {
-  var num = 0;
+  console.log('num ', num);
+  
+  displayQuestion(quizArray[num]);
+  num++;
+  return 0;
+
+  // var num = 0;
   // for (var i = 0; i < questions.length; i++) {
   // passes current queston as parameter, gets answer,
-  displayQuestion(questions[num]);
-  num++;
+  //works
   // passes answer as parameter for verify(), and binds to result
   //var result = verify(displayQuestion(questions[i]));
 
   //}
-  return 0;
 }
-quizSelect();
+//quizSelect();
 
 startQuiz();

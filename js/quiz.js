909a1d6586764991dac111dccac01cbede1934cd
Ian Smith <ianmsmith17@gmail.com>
2019-04-03 13:13:03 -0500

    added quiz page layout

--- js/quiz.js
+++ js/quiz.js
@@ -3,9 +3,11 @@
 var correct = 0;
 var incorrect = 0;
 
+// binds to HTML 'main' elements; used for switching display
 var selectDisplay = document.getElementById('select');
 var questionDisplay = document.getElementById('question');
 
+// array of questions for the text quiz
 var textArray = [
   {text: 'Question 1', answer: 'Correct', options: ["Incorrect", "Incorrect", "Incorrect"]},
   {text: 'Question 2', answer: 'Correct', options: ["Incorrect", "Incorrect", "Incorrect"]},
@@ -13,18 +15,24 @@ var textArray = [
   {text: 'Question 4', answer: 'Correct', options: ["Incorrect", "Incorrect", "Incorrect"]}
 ];
 
+// array of questions for the audio quiz
 var audioArray = [
   {src: '', answer: '', options: []},
   {src: '', answer: '', options: []},
   {src: '', answer: '', options: []},
-  {src: '', answer: '', options: []},
+  {src: '', answer: '', options: []}
 ];
 
+// stores one of the above arrays, depending on which one was selected
+var activeArray;
+
+// results array, stores data from each question and will be stored locally
 var results = [];
 
 // get object properties and display on page (return answer property of object)
 function displayQuestion(question) {
-  var text = question.text;
+  console.log('display');
+
   // code to display on page will go here
   return question.answer;
 }
@@ -42,26 +50,34 @@ function displayStats() {
 
 }
 
-function thisResult() {
-
-}
-
-// main function
+// main function, contains question loop
 function startQuiz(quizType) {
   console.log(quizType);
-  
+  // main question loop, directs complex processes to other functions/subroutines
+  for (var i = 0; i < activeArray.length; i++) {
+    displayQuestion(activeArray[i]);
+  }
 
 }
 
+// calls startQuiz with argument 'text' and changes display
 function selectText() {
+  activeArray = textArray;
   startQuiz('text');
-  
+
+  selectDisplay.style.display = 'none';
+  questionDisplay.style.display = 'block';
 }
 
+// calls startQuiz with argument 'audio' and changes display
 function selectAudio() {
+  activeArray = audioArray;
   startQuiz('audio');
+
+  selectDisplay.style.display = 'none';
+  questionDisplay.style.display = 'block';
 }
 
-
+// event listeners for quiz select divs
 document.getElementById('textQuiz').addEventListener('click', selectText);
 document.getElementById('audioQuiz').addEventListener('click', selectAudio);
